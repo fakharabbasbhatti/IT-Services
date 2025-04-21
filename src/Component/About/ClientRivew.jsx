0b36e1744c47b88b7df887c72202707f7218e42c
raihamza86 <rahamza146@gmail.com>
2025-04-21 13:08:18 +0500

    make icon animate

--- src/Component/About/ClientRivew.jsx
+++ src/Component/About/ClientRivew.jsx
@@ -77,7 +77,7 @@ const StatCard = ({ title, count, suffix, icon, bg }) => {
       className="group flex flex-col items-center text-center md:items-start md:text-left space-y-3"
     >
       <div
-        className={`w-16 h-16 sm:w-20 sm:h-20 flex items-center justify-center rounded-full ${bg} transition-transform duration-500 group-hover:rotate-[360deg] mx-auto`}
+        className={`w-16 h-16 sm:w-20 sm:h-20 flex items-center justify-center rounded-full ${bg} transition-transform duration-500 group-hover:rotate-y-180 mx-auto`}
       >
         {icon}
       </div>
